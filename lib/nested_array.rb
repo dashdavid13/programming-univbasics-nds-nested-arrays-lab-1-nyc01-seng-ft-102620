e2ce0d1d93380cc59a978494f5c7971acf5160cd
David Medina <davidamedina619@gmail.com>
2020-10-12 20:42:17 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,9 +19,9 @@ CONVENTIONAL_PRODUCE = [
 def assembled_matrix
   storage_room = [
     
-    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"] ,
-    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"]
-    
+   
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
     ]
   
   
