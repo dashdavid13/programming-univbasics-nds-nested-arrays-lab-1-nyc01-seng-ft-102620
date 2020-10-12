2fb66fa5cf2c657d91589f33f82ce7e77809d8ec
David Medina <davidamedina619@gmail.com>
2020-10-12 20:50:47 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,7 @@ def sorted_matrix
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
     ]
   storage_room.sort 
-end 
+
   
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
